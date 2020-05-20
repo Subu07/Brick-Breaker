1c451dc4c9f196703cef56c1f8ec6581104f5042
“Sujan” <sujanbudhathoki123@gmail.com>
2020-05-20 15:49:47 +0545

    Added game loop

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,20 @@ let ctx = canvas.getContext("2d");
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-ctx.clearRect(0,0,800,600);
-
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 paddle.draw(ctx);
+
+let lastTime = 0;
+
+const gameLoop = (timeStamp) => {
+  let deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+
+  ctx.clearRect(0, 0, 800, 600);
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+};
+
+gameLoop();
